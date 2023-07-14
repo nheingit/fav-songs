c524a44d4ae6f7f66b1ac816df55b3d55dfe5f94
nheingit <NHeinDev@Gmail.com>
2023-07-14 00:25:53 -0500

    got adding working

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import { Web5 } from '@tbd54566975/web5'
-const queryRecords = async (web5) => {
+
+async function queryRecords(web5) {
     try {
         const { records } = await web5.dwn.records.query({
             message: {
@@ -23,100 +24,104 @@ const queryRecords = async (web5) => {
         console.warn(e)
     }
 }
-async function addRecords(records) {
+
+async function addRecords(web5, records) {
   const RecordData = {
     completed : false,
       records
   };
 
-
-  // Create the record in DWN
-  const { record } = await web5.dwn.records.create({
-    data    : RecordData
-    message : {
-      schema     : 'https://schema.org/MusicRecording',
-      dataFormat : 'application/json'
-    }
-  });
+  try {
+    // Create the record in DWN
+    const { record } = await web5.dwn.records.create({
+      data    : RecordData,
+      message : {
+        schema     : 'https://schema.org/MusicRecording',
+        dataFormat : 'application/json'
+      }
+    });
     console.log('created record successfully: ', record)
+  } catch(e) {
+    console.warn(e)
+  }
 }
 
-function App() {
+export default function App() {
   const [web5state, setWeb5state] = useState({})
-    const [songs, setSongs] = useState([])
-    useEffect(() => {
-        const createWeb5 = async () => {
-            const { web5, did: userDid } = await Web5.connect();
-            setWeb5state({web5, userDid})
-            let data = await queryRecords(web5)
-            console.log(data)
-        }
-        createWeb5()
-    }, [])
+  const [songs, setSongs] = useState([])
+  useEffect(() => {
+      const createWeb5 = async () => {
+          const { web5, did: userDid } = await Web5.connect();
+          setWeb5state({web5, userDid})
+          let data = await queryRecords(web5)
+          console.log(data)
+      }
+      createWeb5()
+  }, [])
 
-    const [inputValues, setInputValues] = useState({
+  const [inputValues, setInputValues] = useState({
     record1: { artist: '', song: '' },
     record2: { artist: '', song: '' },
     record3: { artist: '', song: '' },
   });
 
-    const handleSubmit = async (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    await addRecords(inputValues);
+    const records = Object.values(inputValues);
+    await addRecords(web5state.web5, records);
     setInputValues({
       record1: { artist: '', song: '' },
       record2: { artist: '', song: '' },
       record3: { artist: '', song: '' },
     });  // Clear the input fields
   }
-    const handleInputChange = (event) => {
-    const group = event.target.dataset.group;
-    const key = event.target.name;
+
+  const handleInputChange = (event) => {
+    const idParts = event.target.id.split('_');
+    const recordNumber = idParts[0];
+    const key = idParts[1];
     setInputValues(prevState => ({
       ...prevState,
-      [group]: {
-        ...prevState[group],
+      [recordNumber]: {
+        ...prevState[recordNumber],
         [key]: event.target.value
       }
     }));
   }
 
   return (
-<>
-    <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
-        <div className="relative py-3 sm:max-w-xl sm:mx-auto">
-            <div className="absolute inset-0 bg-gradient-to-r from-cyan-400 to-light-blue-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
-            <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
-                <h1 className="text-2xl font-bold">Welcome to Web5</h1>
-                <form onSubmit={handleSubmit} className="space-y-4">
-                    <div>
-                        <label htmlFor="song1" className="text-sm font-medium">Favorite Song 1:</label>
-                        <input value={inputValues.record1.song} type="text" id="song1" name="song1" className="w-full p-2 border border-gray-300 rounded mt-1" />
-                        <label htmlFor="artist1" className="text-sm font-medium">Artist 1:</label>
-                        <input value={inputValues.record1.artist} type="text" id="artist1" name="artist1" className="w-full p-2 border border-gray-300 rounded mt-1" />
-                    </div>
-
-                    <div>
-                        <label htmlFor="song2" className="text-sm font-medium">Favorite Song 2:</label>
-                        <input value={inputValues.record2.song} type="text" id="song2" name="song2" className="w-full p-2 border border-gray-300 rounded mt-1" />
-                        <label htmlFor="artist2" className="text-sm font-medium">Artist 2:</label>
-                        <input value={inputValues.record2.artist} type="text" id="artist2" name="artist2" className="w-full p-2 border border-gray-300 rounded mt-1" />
-                    </div>
-
-                    <div>
-                        <label htmlFor="song3" className="text-sm font-medium">Favorite Song 3:</label>
-                        <input value={inputValues.record3.song} type="text" id="song3" name="song3" className="w-full p-2 border border-gray-300 rounded mt-1" />
-                        <label htmlFor="artist3" className="text-sm font-medium">Artist 3:</label>
-                        <input value={inputValues.record3.artist} type="text" id="artist3" name="artist3" className="w-full p-2 border border-gray-300 rounded mt-1" />
-                    </div>
-
-                    <input type="submit" value="Submit" className="w-full px-3 py-2 text-white bg-indigo-500 rounded-md hover:bg-indigo-600 focus:outline-none" />
-                </form>
+      <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
+      <div className="relative py-3 sm:max-w-xl sm:mx-auto">
+        <div className="absolute inset-0 bg-gradient-to-r from-cyan-400 to-light-blue-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
+        <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
+          <h1 className="text-2xl font-bold">Welcome to Web5</h1>
+          <form onSubmit={handleSubmit} className="space-y-4">
+            <div>
+              <label htmlFor="record1_artist" className="text-sm font-medium">Artist 1:</label>
+              <input id="record1_artist" type="text" onChange={handleInputChange} value={inputValues.record1.artist} className="w-full p-2 border border-gray-300 rounded mt-1" />
+              <label htmlFor="record1_song" className="text-sm font-medium">Favorite Song 1:</label>
+              <input id="record1_song" type="text" onChange={handleInputChange} value={inputValues.record1.song} className="w-full p-2 border border-gray-300 rounded mt-1" />
             </div>
+
+            <div>
+              <label htmlFor="record2_artist" className="text-sm font-medium">Artist 2:</label>
+              <input id="record2_artist" type="text" onChange={handleInputChange} value={inputValues.record2.artist} className="w-full p-2 border border-gray-300 rounded mt-1" />
+              <label htmlFor="record2_song" className="text-sm font-medium">Favorite Song 2:</label>
+              <input id="record2_song" type="text" onChange={handleInputChange} value={inputValues.record2.song} className="w-full p-2 border border-gray-300 rounded mt-1" />
+            </div>
+
+            <div>
+              <label htmlFor="record3_artist" className="text-sm font-medium">Artist 3:</label>
+              <input id="record3_artist" type="text" onChange={handleInputChange} value={inputValues.record3.artist} className="w-full p-2 border border-gray-300 rounded mt-1" />
+              <label htmlFor="record3_song" className="text-sm font-medium">Favorite Song 3:</label>
+              <input id="record3_song" type="text" onChange={handleInputChange} value={inputValues.record3.song} className="w-full p-2 border border-gray-300 rounded mt-1" />
+            </div>
+
+            <button type="submit" className="w-full px-3 py-2 text-white bg-indigo-500 rounded-md hover:bg-indigo-600 focus:outline-none">Submit</button>
+          </form>
         </div>
+      </div>
     </div>
-</>
   )
 }
 
-export default App
